db838f16e1e3a477822ab8fa2642a9e61f4694fb
Dozie Udeagha <dozie.udeagha@gmail.com>
2024-06-20 15:53:09 +0100

    Update styling for top navbar tablet

--- app/components/NavBar/TopBar.tsx
+++ app/components/NavBar/TopBar.tsx
@@ -1,28 +1,44 @@
 import Image from "next/image";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { IoSearchOutline } from "react-icons/io5";
 import styles from "./navBar.module.scss";
 import bell from "../../../public/assets/np_notification_2425223_000000.png";
 import userPix from "../../../public/assets/user-pix.png";
 import dropDown from "../../../public/assets/np_dropdown_615120_0000001.png";
 import { TopBarProps } from "../../../types";
 
-
-
 const TopBar = ({ isCollapsed, setIsCollapsed }: TopBarProps) => {
   return (
     <header className={styles.main_container}>
       <div className={styles.container}>
         {isCollapsed ? (
-          <div className={styles.hamburger} onClick={() => setIsCollapsed(false)}>
+          <div
+            className={styles.hamburger}
+            onClick={() => setIsCollapsed(false)}
+          >
             <IoMdMenu size={30} />
           </div>
         ) : (
-          <div className={styles.hamburger} onClick={() => setIsCollapsed(true)}>
+          <div
+            className={styles.hamburger}
+            onClick={() => setIsCollapsed(true)}
+          >
             <IoMdClose size={20} />
           </div>
         )}
+
         <div className={styles.logo_container}>
           <div className={styles.logo} />
+        </div>
+        <div className={styles.search_container}>
+          <input
+            type="text"
+            placeholder="Search for anything"
+            className={styles.search_input}
+          />
+          <div className={styles.search_icon}>
+            <IoSearchOutline color="white" size={24}/>
+          </div>
         </div>
         <div className={styles.nav_items}>
           <div className={styles.docs_container}>
