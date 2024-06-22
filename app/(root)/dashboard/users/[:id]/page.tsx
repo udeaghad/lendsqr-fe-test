d1fe46ee56568f64ce922521ca47bc566e70e735
Dozie Udeagha <dozie.udeagha@gmail.com>
2024-06-22 13:54:43 +0100

    Create dynamic routing for user details

---
+++ app/(root)/dashboard/users/[:id]/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../../dashboard.module.scss'
+
+const UserDetails = () => {
+  return (
+    <div className={styles.container}>UserDetails</div>
+  )
+}
+
+export default UserDetails
