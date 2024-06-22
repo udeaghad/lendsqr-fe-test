4a16fcf6d6c773c4a240b216e86707871ec11603
Dozie Udeagha <dozie.udeagha@gmail.com>
2024-06-22 14:15:46 +0100

    Fix styling of filter dropdown for desktop

--- app/(root)/dashboard/users/[:id]/page.tsx
+++ app/(root)/dashboard/users/[:id]/page.tsx
@@ -1,9 +1,41 @@
-import styles from '../../dashboard.module.scss'
+import Image from "next/image";
+import styles from "../../dashboard.module.scss";
+import UserProfileCard from "@/app/components/UserProfile/UserProfileCard";
 
 const UserDetails = () => {
   return (
-    <div className={styles.container}>UserDetails</div>
-  )
-}
+    <div className={styles.container}>
+      <div>
+        <div>
+          <Image
+            src="/assets/np_back_3007750_000000.png"
+            alt="user icon"
+            width={30}
+            height={30}
+          />
+        </div>
+        <div>
+          <span>Back to Users</span>
+        </div>
+      </div>
 
-export default UserDetails
+      <div>
+        <span>User Details</span>
+      </div>
+
+      <div>
+        <span>Blacklist User</span>
+      </div>
+
+      <div>
+        <span>Activate User</span>
+      </div>
+
+      <div>
+        <UserProfileCard/>
+      </div>
+    </div>
+  );
+};
+
+export default UserDetails;
---
+++ app/components/UserProfile/UserProfileCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserProfileCard = () => {
+  return (
+    <div>UserProfileCard</div>
+  )
+}
+
+export default UserProfileCard
