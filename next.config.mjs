5022552f1486ff80b03b47521d6574d9fe9c070a
Dozie Udeagha <dozie.udeagha@gmail.com>
2024-06-22 09:35:57 +0100

    Update login as the root route

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
+import path from "path";
 
 const __dirname = path.resolve();
 
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
 
